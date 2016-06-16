380146d07331eb4706ec4c00089b2400eabde288
Tiffany <tiffanyphan1221@gmail.com>
2016-06-15 22:15:49 -0400

    minor changes

--- mat2py.py
+++ mat2py.py
@@ -4,9 +4,9 @@ import h5py
 import os
 import sys 
 
-# mat2py imports a parameter from a MATLAB struct into a Python ndarray 
-# mfile the struct's name e.g. ('test.mat')
-# p is the desired parameter e.g. ('y_pstd')
+# mat2py imports a parameter from a MATLAB struct and returns it as a ndarray 
+# mfile the struct's name e.g. (test.mat)
+# p is the desired parameter e.g. (y_pstd)
 
 def mat2py(mfile,p):
 
@@ -20,24 +20,20 @@ def mat2py(mfile,p):
 
 # data is a list of ndarrays for every track of specified parameter p
 	data = [gp1[element[0]][:] for element in gp1[p]];
-	#t = len(gp1.get(p));
-
-	#data = [gp1[element[0]][:] for element in gp1[44]];
-	#data.append(gp1[element[t]][:] for element in gp1[p])
-	# i = 0;
-	# for d in data: 
-	# 	if d == None: 
-	# 		return i;
-	# 	i += 1; 
+	#t = len(gp1.get('t'));
+	#data.append(gp1[element[t]][:] for element in gp1['t'])
 
 # arr is an ndarray of ndarrays 
 	arr = np.array(data);
-	print (arr);
-	#return arr;
+	return arr;
 
 if __name__ == "__main__": 
 	mat2py(sys.argv[1], sys.argv[2]);
 
+
+
+
+# EXTRA STUFF # 
 # Finds mfile's path, if you don't know where it is
 	# for r,d,f in os.walk("C:\\"):
  #    	for files in f:
