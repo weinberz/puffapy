add2d2aa45e7d55dd47a0f054c7f4ac45fd4bbd4
Zach Weinberg <weinberz@gmail.com>
2016-06-21 19:46:46 -0400

    Added argparse and exception handling to mat2py. Added gitignore for ignoring .mat files.

--- mat2py.py
+++ mat2py.py
@@ -1,49 +1,41 @@
-import numpy as np 
-import scipy.io as sio
-import h5py 
+import argparse
 import os
-import sys 
+import sys
 
-# mat2py imports a parameter from a MATLAB struct and returns it as a ndarray 
+import h5py
+import numpy as np
+import scipy.io as sio
+
+# mat2py imports a parameter from a MATLAB struct and returns it as a ndarray
 # mfile the struct's name e.g. (test.mat)
 # p is the desired parameter e.g. (y_pstd)
 
 def mat2py(mfile,p):
-
-
 # Finds mfile's path if it's in current dir
 	mfilepath = os.path.abspath(mfile);
 
-# Imports the MATLAB struct and retrieves tracks (HP5 group type)
-	f = h5py.File(mfile); 
-	gp1 = f.get('tracks');
+	# Imports the MATLAB struct and retrieves tracks (HP5 group type)
+	try:
+		f = h5py.File(mfile,'r');
+		gp1 = f.get('tracks');
 
-# data is a list of ndarrays for every track of specified parameter p
-	data = [gp1[element[0]][:] for element in gp1[p]];
-	#t = len(gp1.get('t'));
-	#data.append(gp1[element[t]][:] for element in gp1['t'])
+		# data is a list of ndarrays for every track of specified parameter p
+		data = [gp1[element[0]][:] for element in gp1[p]];
+		#t = len(gp1.get('t'));
+		#data.append(gp1[element[t]][:] for element in gp1['t'])
 
-# arr is an ndarray of ndarrays 
+
+	except OSError:
+		print("Woops, old matlab format. This will fail.")
+
+	# arr is an ndarray of ndarrays
 	arr = np.array(data);
+
 	return arr;
 
-if __name__ == "__main__": 
-	mat2py(sys.argv[1], sys.argv[2]);
-
-
-
-
-# EXTRA STUFF # 
-# Finds mfile's path, if you don't know where it is
-	# for r,d,f in os.walk("C:\\"):
- #    	for files in f:
- #        	 if files == mfile:
- #              	mfilepath == os.path.join(r,files)
-
-
-# Retrieves list of parameter names for reference 
-# (Not super necessary but nice to have)
-	# param = [];
-	# for item in gp1.keys():
-	# 	param = param + [item];
-
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Convert .mat files to numpy arrays for use with randomforest.py")
+	parser.add_argument("matfile", help="The file to be parsed")
+	parser.add_argument("fields", nargs="+", help="Field(s) to extract from .mat file")
+	args = parser.parse_args()
+	mat2py(args.matfile,args.fields);
